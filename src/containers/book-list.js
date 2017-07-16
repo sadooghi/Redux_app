5003252aea79db30a49a62d3146c9a84e4b72dd2
sadooghi <gh.sadooghi@gmail.com>
2017-07-16 11:47:22 -0400

    book select and show details

---
+++ src/containers/book-list.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { selectBook } from '../actions/index';
+import { bindActionCreators } from 'redux';
+
+class BookList extends Component {
+  renderList(){
+    return this.props.books.map((book) => {
+      return (
+        <li
+        key={book.title}
+        onClick={()=>{this.props.selectBook(book)}}
+        className="list-gorup-item">
+        {book.title}
+        </li>
+      );
+    });
+  }
+  render() {
+    return(
+      <ul className="list-gorup col-sm-4">
+        {this.renderList()}
+      </ul>
+    )
+  }
+}
+
+function mapStateToProps (state) {
+  //whatever is returned will show up as props inside BookList
+  return {
+    books: state.books
+  };
+}
+
+//Anything returned from this function will end up as props on BookList container
+function mapDispatchToProps (dispatch) {
+  //whenever selectBook is called the result should be passed to all of our reducers
+  return bindActionCreators({selectBook: selectBook}, dispatch);
+}
+
+//promote BookList from a component to a container. It needs to know about this dispatch methode, selectBook
+//Make it available as a prop
+
+export default connect(mapStateToProps, mapDispatchToProps)(BookList);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,11 @@
 import { combineReducers } from 'redux';
+import BooksReducer from './reducer_books';
+import ActiveBook from './reducer_active_book';
 
 const rootReducer = combineReducers({
-  state: (state = {}) => state
+  //any key to the object that we provide to the combineReducers ends up as a key on our global state
+  books: BooksReducer,
+  activeBook: ActiveBook
 });
 
 export default rootReducer;
